4daaf1823e65f14e7793e6820a0d57bc3c4f239c
Ali Reza <rezatrue@gmail.com>
2023-05-21 07:45:52 +0600

    21May2023h

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,20 +1,35 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-from pages.accountCreatedPage import AccountCreatedPage
-from browserAction import BrowserAction
-from pages.deleteAccountPage import DeleteAccountPage
-from pages.homepage import HomePage
-from pages.loginPage import LoginPage
-from pages.signupPage import SignupPage
-
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 @pytest.fixture(scope="class")
-def setup(request):
-    opts = webdriver.ChromeOptions()
-    opts.add_argument("--start-maximized")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+def setup(request, browser, url):
+    global driver
+    if browser == "chrome":
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser == "edge":
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    driver.get(url)
+    driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.close()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--url")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def url(request):
+    return request.config.getoption("--url")
